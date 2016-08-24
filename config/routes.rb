c2c5975b5ac1a7beedb712b8565952bd1b3aab46
Lia <lia@shopstudio-oro.com>
2016-08-24 03:19:00 +0000

    Added Contact form CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
   resources :projects
- 
+  resources :contacts
+  
+  get 'contacts/index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160824002539) do
+ActiveRecord::Schema.define(version: 20160824030303) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
