72d30c6c1f336f2d48f51a41b86a2df2e66371f7
Lia <lia@shopstudio-oro.com>
2016-09-12 18:04:28 +0000

    adjusted controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts
-  resources :projects
+  
+    resources :projects do
+    end
+    
   resources :contacts
+  resources :posts
   
   
   get 'welcome/index'
